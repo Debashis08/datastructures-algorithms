a88931d06e464ddef6b0c32b48e430649001c3e4
Debashis Nandi <debashisnandi576@gmail.com>
2025-01-14 00:47:16 +0530

    feature: diff constraints initial changes

---
+++ SourceCodes/0003_Graph/0012_DifferenceConstraintsShortestPaths.cc
@@ -0,0 +1,62 @@
+#include"../Headers/0003_Graph/0012_DifferenceConstraintsShortestPaths.h"
+#include<climits>
+using namespace std;
+
+namespace DifferenceConstraintsShortestPaths
+{
+	Node::Node(int data)
+	{
+		this->data = data;
+		this->distance = INT_MAX;
+	}
+
+	Edge::Edge(Node* nodeU, Node* nodeV, int weight)
+	{
+		this->nodeU = nodeU;
+		this->nodeV = nodeV;
+		this->weight = weight;
+	}
+
+	// Graph Private Member Methods
+	Node* Graph::MakeOrFindNode(int data)
+	{
+		Node* node = nullptr;
+		if (this->_nodeMap.find(data) == this->_nodeMap.end())
+		{
+			node = new Node(data);
+			this->_nodeMap[data] = node;
+		}
+		else
+		{
+			node = this->_nodeMap[data];
+		}
+		return node;
+	}
+
+	void Graph::InitializeSingleSource(Node* sourceNode)
+	{
+		for (auto& iterator : this->_nodeMap)
+		{
+			iterator.second->distance = INT_MAX;
+		}
+		sourceNode->distance = 0;
+	}
+
+	void Graph::Relax(Edge* edge)
+	{
+		if (edge->nodeU->distance != INT_MAX && (edge->nodeV->distance > (edge->nodeU->distance + edge->weight)))
+		{
+			edge->nodeV->distance = edge->nodeU->distance + edge->weight;
+		}
+	}
+
+	// Graph Public Member Methods
+	void Graph::PushDirectedEdge(int dataU, int dataV, int weight)
+	{
+		Node* nodeU = this->MakeOrFindNode(dataU);
+		Node* nodeV = this->MakeOrFindNode(dataV);
+
+		this->_adjlist[nodeU].push_back(nodeV);
+		this->_edgeList.push_back(new Edge(nodeU, nodeV, weight));
+	}
+}
