917c05d83e4ae81ae699c9357e03165f1e522f0d
Debashis Nandi <debashisnandi576@gmail.com>
2025-07-30 22:10:49 +0530

    feature: dp pattern 1 fibonacci, tribonacci added

---
+++ include/0005_DynamicProgramming/0002_TribonacciNumber.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<vector>
+using namespace std;
+
+/*
+Pattern 1
+Linear Recurrence
+
+Description
+Print the n'th Tribonacci number.
+
+*/
+
+namespace TribonacciNumber
+{
+	class DynamicProgramming
+	{
+	private:
+	public:
+		int RecursiveNthTribonacci(int n);
+		int DpNthTribonacci(int n);
+	};
+}
---
+++ source/0005_DynamicProgramming/0001_FibonacciNumber.cc
@@ -0,0 +1,28 @@
+#include "../../include/0005_DynamicProgramming/0001_FibonacciNumber.h"
+
+namespace FibonacciNumber
+{
+	int DynamicProgramming::RecursiveNthFibonacci(int n)
+	{
+		if (n <= 1)
+		{
+			return n;
+		}
+
+		return this->RecursiveNthFibonacci(n - 1) + this->RecursiveNthFibonacci(n - 2);
+	}
+
+	int DynamicProgramming::DpNthFibonacci(int n)
+	{
+		vector<int> dp(n + 1, 0);
+		dp[0] = 0;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++)
+		{
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
+	}
+}
