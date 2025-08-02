b707e7c2ccbbe2070274449f86eb5fea1b9afd5d
Debashis Nandi <debashisnandi576@gmail.com>
2025-08-02 23:56:12 +0530

    feature: climbing stairs added

---
+++ source/0005_DynamicProgramming/0003_ClimbingStairs.cc
@@ -0,0 +1,29 @@
+#include "../../include/0005_DynamicProgramming/0003_ClimbingStairs.h"
+using namespace std;
+
+namespace ClimbingStairs
+{
+	int DynamicProgramming::RecursiveCountWays(int n)
+	{
+		if (n == 0 || n == 1)
+		{
+			return 1;
+		}
+
+		return this->RecursiveCountWays(n - 1) + this->RecursiveCountWays(n - 2);
+	}
+
+	int DynamicProgramming::DpCountWays(int n)
+	{
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; i++)
+		{
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
+	}
+}
