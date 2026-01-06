953a660f92f35a069232729f4aae649940d5c6cd
Debashis Nandi <debashisnandi576@gmail.com>
2026-01-07 02:38:41 +0530

    feat: knapsack recursive solution added

--- source/0005_DynamicProgramming/0013_KnapsackProblem.cc
+++ source/0005_DynamicProgramming/0013_KnapsackProblem.cc
@@ -3,12 +3,30 @@
 namespace KnapsackProblem
 {
 	// Dynamic Programming Private Member Methods.
-	
+	int DynamicProgramming::KnapsackRecursiveHelper(int capacity, vector<int>& weight, vector<int>& profit, int numberOfItems)
+	{
+		if (capacity <= 0 || numberOfItems == 0)
+		{
+			return 0;
+		}
+		
+		int pickCurrentItem = 0;
+		
+		// Pick the current item only if does not exceed the capacity.
+		if (weight[numberOfItems - 1] <= capacity)
+		{
+			pickCurrentItem = profit[numberOfItems - 1] + this->KnapsackRecursiveHelper(capacity - weight[numberOfItems - 1], weight, profit, numberOfItems - 1);
+		}
 
+		int dropCurrentItem = this->KnapsackRecursiveHelper(capacity, weight, profit, numberOfItems - 1);
+
+		return max(pickCurrentItem, dropCurrentItem);
+	}
 
 	// Dynamic Programming Public Member Methods.
 	int DynamicProgramming::RecursiveKnapsack(int capacity, vector<int> weight, vector<int> profit)
 	{
-		return 0;
+		size_t totalNumberOfItems = weight.size();
+		return this->KnapsackRecursiveHelper(capacity, weight, profit, totalNumberOfItems);
 	}
 }
---
+++ test/0005_DynamicProgramming/0013_KnapsackProblemTest.cc
@@ -0,0 +1,21 @@
+#include<gtest/gtest.h>
+#include "../../include/0005_DynamicProgramming/0013_KnapsackProblem.h"
+
+namespace KnapsackProblem
+{
+	TEST(KnapsackProblemTest, RecursiveKnapsackProblemTest01)
+	{
+		// Arrange
+		DynamicProgramming dp;
+		int capacity = 4;
+		vector<int> weight = { 4,5,1 };
+		vector<int> profit = { 1,2,3 };
+		int expectedMaximumProfit = 3;
+
+		// Act
+		int actualMaximumProfit = dp.RecursiveKnapsack(capacity, weight, profit);
+
+		// Assert
+		ASSERT_EQ(expectedMaximumProfit, actualMaximumProfit);
+	}
+}
